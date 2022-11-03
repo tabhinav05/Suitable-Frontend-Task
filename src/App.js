1a87e04de12e753ec1ac2b2750b2c6506e444227
tabhinav05 <abhinav.tanwar@connvertex.com>
2022-11-03 17:16:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { RealData } from './data';
+import { Layout, Typography, Input, Col, Row, Space, Table, Tag, Avatar, List, Steps, Button } from 'antd';
+import { FireOutlined } from '@ant-design/icons';
+import "antd/dist/antd.css";
+import { useMediaQuery } from 'react-responsive'
+
+const { Header, Footer, Content } = Layout;
+const { Title } = Typography;
+
+
 
 function App() {
+
+  const onSearch = (event) => {
+    const filteredData = RealData.filter((data) => {
+      return data.title.toLowerCase().includes(event.target.value);
+    })
+    setData(filteredData);
+  };
+  const [data, setData] = useState(RealData)
+
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 823px)' });
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header style={{ background: '#2dcc3f', height: "40vh", position: "Absolute", width: "100%" }}>
+        <Row justify="center">
+          <Col xs={24} sm={24} md={24} lg={22} xl={20} >
+
+            <Title style={{ color: 'white', marginTop: '30px' }}>Referral Status <FireOutlined /></Title>
+            <Input
+              placeholder="Search"
+              allowClear
+              onChange={onSearch}
+              style={{ maxWidth: "340px", borderRadius: '5px' }}
+              size="large"
+            />
+          </Col>
+        </Row>
+      </Header>
+      {/* <Row justify="center"   >
+        <Col xs={24} sm={24} md={22} lg={20} xl={20} style={{ background: 'white', top: "180px", position: "relative", borderRadius: "6px", overflowX: "scroll" }} >
+          <List
+            itemLayout="vertical"
+            size='large'
+            style={{ width: "100%" }}
+            grid={{
+              xxl: RealData.length
+            }}
+
+            header={
+              <Row >
+                <Col flex="230px" style={{ display: "flex", justifyContent: 'center' }}>
+                  <Title level={4}>Candidate</Title>
+                </Col>
+                <Col flex="auto">
+                  <Row >
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Reffered</Title>
+                    </Col>
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Interviewed</Title>
+                    </Col>
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Hired</Title>
+                    </Col>
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Joined</Title>
+                    </Col>
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Reward</Title>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>}
+            dataSource={data}
+            renderItem={item => (
+              <div>
+                <Avatar style={{ marginLeft: "10px", width: "45px", height: "auto" }} src="https://joeschmoe.io/api/v1/random" />
+                <Typography.Title level={5}>{item.title}</Typography.Title>
+                <Steps style={{ height: '78vh' }} direction="vertical" labelPlacement="vertical" items={item.items} />
+
+              </div>
+
+            )}
+          />
+
+        </Col>
+      </Row> */}
+
+
+      <Row justify="center"   >
+        <Col xs={24} sm={24} md={24} lg={22} xl={20} style={{ background: 'white', top: "180px", position: "relative", borderRadius: "6px" }} >
+
+          <List
+            itemLayout="vertical"
+            header={!isTabletOrMobile? 
+              <Row >
+                <Col flex="240px" style={{ display: "flex", justifyContent: 'center' }}>
+                  <Title level={4}>Candidate</Title>
+                </Col>
+                <Col flex="auto">
+                  <Row >
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Reffered</Title>
+                    </Col>
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Interviewed</Title>
+                    </Col>
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Hired</Title>
+                    </Col>
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Joined</Title>
+                    </Col>
+                    <Col flex={1} style={{ display: "flex", justifyContent: 'center' }}>
+                      <Title level={4}>Reward</Title>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>: null}
+            dataSource={data}
+            renderItem={item => (
+              <List.Item>
+                <div style={{ width: "100%" }}>
+
+                  <Row style={{ margin: "0 1%" }}>
+                    <Col flex="210px" style={{ display: "flex", justifyContent: 'start', alignItems: 'center' }}   >
+                      <Avatar style={{  width: "45px", height: "auto" }} src="https://joeschmoe.io/api/v1/random" />
+                      <Typography.Title level={5}>{item.title}</Typography.Title>
+
+                    </Col>
+                    <Col flex="auto" >
+                      <Steps labelPlacement="vertical" direction={isTabletOrMobile?"vertical":"horizontal"} style={{ color: 'green' }} items={item.items} />
+                    </Col>
+
+                  </Row>
+                </div>
+
+
+              </List.Item>
+
+            )}
+          />
+
+         </Col>
+      </Row> 
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,326 @@
+import { Typography} from 'antd';
+const { Title } = Typography;
+
+
+export  const RealData = [
+    {
+      title: 'Anmol Mahajan',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered",
+        },
+        {
+          status: 'finish',
+          description:'Interviewed'
+  
+        },
+        {
+          status: 'finish',
+          description:'Hired'
+  
+  
+        },
+        {
+          status: 'finish',
+          description:"Joined"
+  
+  
+        },
+        {
+          status: "process",
+          icon: <Title level={3}style={{ color: '#ffcc00',width:"80px"}}><b>$</b>2000</Title>
+  
+        },
+      ]
+    },
+    {
+      title: 'Abhinav Tanwar',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered"
+
+  
+        },
+        {
+          status: 'finish',
+          description:'Interviewed'
+
+  
+        },
+        {
+          status: 'finish',
+          description:'Hired'
+
+        },
+        {
+          status: 'error',
+          description:"Joined"
+
+        },
+        {
+          icon: <Title level={3} style={{ width:"80px"}} disabled><b>$</b>2000</Title>
+  
+        },
+      ]
+    },
+    {
+      title: 'Rohan ',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered"
+
+  
+        },
+        {
+          status: 'finish',
+          description:'Interviewed'
+
+        },
+        {
+          status: 'process',
+          description:'Hired'
+  
+        },
+        {
+            description:"Joined"
+        },
+        {
+          status: "wait",
+          icon: <Title level={3} style={{ width:"80px"}} disabled><b>$</b>2000</Title>
+  
+        },
+      ]
+    },
+    {
+      title: 'Muskan Tanwar',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered"
+
+  
+        },
+        {
+          status: 'finish',
+          description:'Interviewed'
+
+        },
+        {
+          status: 'finish',
+          description:'Hired'
+
+        },
+        {
+          status: 'finish',
+          description:"Joined"
+
+        },
+        {
+          status: "process",
+          icon: <Title level={3} style={{ color: '#ffcc00',width:"80px"}}><b>$</b>2000</Title>
+  
+        },
+      ]
+    },
+    {
+      title: 'Abhinav Tanwar',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered"
+
+  
+        },
+        {
+          status: 'finish',
+          description:'Interviewed'
+
+        },
+        {
+          status: 'finish',
+          description:'Hired'
+
+  
+  
+        },
+        {
+          status: 'error',
+          description:"Joined"
+
+  
+  
+        },
+        {
+          icon: <Title level={3} style={{ width:"80px"}}  disabled><b>$</b>2000</Title>
+  
+        },
+      ]
+    },
+    {
+      title: 'Rohan ',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered"
+
+  
+        },
+        {
+          status: 'process',
+          description:'Interviewed'
+
+        },
+        {
+            description:'Hired'
+
+  
+        },
+        {
+  
+            description:"Joined"
+
+        },
+        {
+          status: "wait",
+          icon: <Title level={3} style={{ width:"80px"}}  disabled><b>$</b>2000</Title>
+  
+        },
+      ]
+    },
+    {
+      title: 'Muskan Tanwar',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered"
+
+  
+        },
+        {
+          status: 'finish',
+          description:'Interviewed'
+
+        },
+        {
+          status: 'finish',
+          description:'Hired'
+
+        },
+        {
+          status: 'process',
+          description:"Joined"
+
+        },
+        {
+          icon: <Title level={3} style={{ width:"80px"}}  disabled><b>$</b>2000</Title>
+  
+  
+        },
+      ]
+    },
+    {
+      title: 'Anmol Mahajan',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered"
+
+        },
+        {
+          status: 'finish',
+          description:'Interviewed'
+
+  
+        },
+        {
+          status: 'finish',
+          description:'Hired'
+
+  
+  
+        },
+        {
+          status: 'finish',
+          description:"Joined"
+
+  
+  
+        },
+        {
+          status: "process",
+          icon: <Title level={3} style={{ color: '#ffcc00',width:"80px"}}><b>$</b>2000</Title>
+  
+        },
+      ]
+    },
+    {
+      title: 'Abhinav Tanwar',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered"
+
+  
+        },
+        {
+          status: 'finish',
+          description:'Interviewed'
+
+  
+        },
+        {
+          status: 'finish',
+          description:'Hired'
+
+  
+  
+        },
+        {
+          status: 'error',
+          description:"Joined"
+
+  
+  
+        },
+        {
+          icon: <Title level={3} style={{ width:"80px"}} disabled><b>$</b>2000</Title>
+  
+        },
+      ]
+    },
+    {
+      title: 'Rohan ',
+      items: [
+        {
+          status: 'finish',
+          description:"Reffered"
+
+  
+        },
+        {
+          status: 'process',
+          description:'Interviewed'
+
+        },
+        {
+            description:'Hired'
+
+  
+        },
+        {
+  
+            description:"Joined"
+
+        },
+        {
+          status: "wait",
+          icon: <Title level={3} style={{ width:"80px"}}  disabled><b>$</b>2000</Title>
+  
+        },
+      ]
+    },
+  ]
+
